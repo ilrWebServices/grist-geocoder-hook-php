273b503f407632b3bd4a652329f78711c03de146
Jeff Amaral <jeff.amaral@cornell.edu>
2025-04-16 09:47:37 -0400

    Restrict build script to running only in a clean working directory

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
+set -e
+
+if ! [ -z "$(git status --porcelain)" ]; then
+  echo 'Please run this script in a clean working directory.' >&2
+  exit 1
+fi
+
 docker build --tag ilrweb/grist-geocoder-hook-php:latest .
+
+exit 0
